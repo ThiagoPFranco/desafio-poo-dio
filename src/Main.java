737f2ef1340ac0c686be0fe5dd418ae9bdcb6dc8
ThiagoPFranco <thiago.p.franco@hotmail.com>
2023-04-12 11:40:08 -0300

    Finalização do desafio Bootcamp Java.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -25,7 +27,35 @@ public class Main {
         System.out.println(curso2);
         System.out.println(mentoria);
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Banco Pan Java Developer");
+        bootcamp.setDescricao("Descrição Banco Pan Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
 
+        Dev devThiago = new Dev();
+        devThiago.setNome("Thiago");
+        devThiago.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Thiago: " + devThiago.getConteudosInscritos());
+        devThiago.progredir();
+        devThiago.progredir();
+        devThiago.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos concluídos Thiago: " + devThiago.getConteudosConcluidos());
+        System.out.println("XP: " + devThiago.calcularXp());
+
+        System.out.println("---------------------------");
+
+        Dev devJuliana = new Dev();
+        devJuliana.setNome("Juliana");
+        devJuliana.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Juliana: " + devJuliana.getConteudosInscritos());
+        devJuliana.progredir();
+        devJuliana.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos concluídos Juliana: " + devJuliana.getConteudosConcluidos());
+        System.out.println("XP: " + devJuliana.calcularXp());
 
 
     }
